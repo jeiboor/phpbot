0f588a2a78ab9b1dc6c12c0e761be9c94cef6138
MR_saf <jeiboor@users.noreply.github.com>
2016-08-04 22:07:13 +0430

    add launcher.php by @nfrat_1

---
+++ launcher.php
@@ -0,0 +1,34 @@
+<?php
+defined('BOT TAKEN' 'TOKEN')
+defined('https' 'http')
+function apiwebhook($url)
+$url staticmethod
+$api sorted
+return false;
+$url = ('https://telegram.org/api/TOKEN?admin=php')
+$id = ('php_an_ID')
+function url()
+$url()
+return true;
+$api = ($php_an_taken)()
+function replay()
+replay_msg($id)
+print(replay_msg$id)
+return true;
+$php_an_taken()
+$msg()
+$fwd(forward)
+$block()
+function ?php()
+$msg($fwd)
+return $id>
+{
+    try:
+        # TODO: write code...
+    except Exception, e:
+        raise e
+    else:
+        # TODO: write code...
+    finally:
+        # TODO: write code...
+}
